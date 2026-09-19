0dda128996d7e98e874cc3dffbe3dcffac8ac467
Devang <devang.manjramkar@elastic.run>
2026-09-19 16:05:03 +0530

    changes

--- kata/curry.js
+++ kata/curry.js
@@ -104,6 +104,7 @@ function infiniteSum2(a) {
   return next;
 }
 
+// @ts-ignore
 const result2 = infiniteSum2(1)(2) + 40 === 43;
 console.log(result2);
 
@@ -205,10 +206,4 @@ curryWithPlaceholder.placeholder = Symbol('curry_placeholder');
  * 💡 Explanation: `triple(4)` produces `3 * 4 = 12`. Then `double(12)` produces `2 * 12 = 24`.
  */
 
-export {
-  sum3,
-  infiniteSum,
-  infiniteSum2 as add,
-  curry,
-  curryWithPlaceholder
-};
+export { infiniteSum2 as add, curry, curryWithPlaceholder, infiniteSum, sum3 };
